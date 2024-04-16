535225bed25f594a804b6e27a91ec6643781c214
marcr123 <166740084+marcr123@users.noreply.github.com>
2024-04-16 11:16:49 -0700

    Update Person2.h

--- Person2.h
+++ Person2.h
@@ -1,31 +1,59 @@
 #pragma once
-#pragma once
-#include<vector>
+#include<iostream>
 
 using namespace std;
 
-class CongressPerson	 {
-public:
+class CongressPerson {
+public:	 
 	CongressPerson();
-	CongressPerson(string firstName, string lastName, string phoneNumber);
-	void Display();
+	CongressPerson(string lastName, string firstName, string middleName, string suffix,
+		string nickname, string fullName, string birthday, string gender,
+		string type, string state, string district, string senateClass, string party,
+		string url, string address, string phoneNumber);
+
+	void Display()const;
+
 	string getFirstName()const;
 	string getLastName()const;
+	string getMiddleName()const;
+	string getSuffix()const;
+	string getNickname()const;
+	string getFullName()const;
+	string getBirthday()const;
+	string getGender()const;
+	string getType()const;
+	string getState()const;
+	string getDistrict()const;
+	string getSenateClass()const;
+	string getParty()const;
+	string getURL()const;
+	string getAddress()const;
 	string getPhoneNumber()const;
+
 	void setFirstName(string firstName);
 	void setLastName(string lastName);
+	void setMiddleName(string middleName);
+	void setSuffix(string suffix);
+	void setNickname(string nickname);
+	void setFullName(string fullName);
+	void setBirthday(string birthday);
+	void setGender(string gender);
+	void setType(string type);
+	void setState(string state);
+	void setDistrict(string district);
+	void setSenateClass(string senateClass);
+	void setParty(string party);
+	void setURL(string url);
+	void setAddress(string address);
 	void setPhoneNumber(string phoneNumber);
+	
 	bool operator < (CongressPerson& rhs )const; //compares phone number
 	bool operator == (CongressPerson& rhs);
 	bool operator >= (const CongressPerson& rhs);
 	CongressPerson& operator = (const CongressPerson& rhs); //copies CongressPerson vals to another CongressPerson
+	bool operator>(const CongressPerson& rhs);	
 private:
-	string firstName;
-	string lastName;
-	string phoneNumber;
-	string middleName, suffix,
-		nickname, fullName, birthday, gender,
-		type, state, district, senateClass, party,
-		url, address;
-
+	string lastName, firstName, middleName, suffix , nickname, fullName , birthday , gender,
+	type , state , district , senateClass , party , url , address, phoneNumber;
 };
+
