cbd3bfa21bb9f3ca0a39ae4f0aac1f4b3c3aef0d
marcr123 <166740084+marcr123@users.noreply.github.com>
2024-04-16 11:15:43 -0700

    Update PhoneBook.cpp

--- PhoneBook.cpp
+++ PhoneBook.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 #include"Person2.h"
 #include"PhoneBook.h"
-#include<vector>
+
 using namespace std;
 
-PhoneBook::PhoneBook()
+PhoneBook::PhoneBook() 
 {
 	numContacts = 0;
 	capacity = 20;
-	personArr = new Person[capacity];
-
+	personArr = new CongressPerson[capacity];
+	
 }
 
 PhoneBook::~PhoneBook() {
@@ -22,56 +22,64 @@ PhoneBook::PhoneBook(const PhoneBook& orgCopy)
 	cout << "Copy const here...";
 	numContacts = orgCopy.numContacts;
 	capacity = orgCopy.capacity;
-	numContacts = orgCopy.numContacts;
+	numContacts = orgCopy.numContacts; 
 
-	personArr = new Person[capacity];
+	personArr = new CongressPerson[capacity];
 
-	for (int i = 0; i < numContacts; i++) {
+	for (int i = 0; i < capacity; i++){
 		personArr[i] = orgCopy.personArr[i];
 	}
 }
 
-void PhoneBook::Add(Person personToAdd)
+void PhoneBook::Add(CongressPerson& personToAdd) 
 {
 	numContacts++;
-	if (numContacts == capacity) {
-		capacity += 20;
+
+	if (numContacts >= capacity) {
+		CongressPerson* temp = new CongressPerson[capacity+1];
+		for (int i = 0; i < capacity; i++) {
+			temp[i] = personArr[i];
+		}
+		delete[] personArr;
+		personArr = temp;
+		capacity++;
+		for (int i = numContacts - 1; i < capacity; i++) {
+			personArr[i] = personToAdd;
+		}
 	}
-	for (int i = numContacts - 1; i < numContacts; i++) {
-		personArr[i] = personToAdd;
+	else {
+
+		for (int i = numContacts - 1; i < capacity; i++) {
+			personArr[i] = personToAdd;
+		}
 	}
+	
+	
 
 }
 
-void PhoneBook::Display()
+void PhoneBook::Display() 
 {
-
+	
 	for (int i = 0; i < numContacts; i++) {
 		personArr[i].Display();
-	}
+	} 
 }
 
-PhoneBook& PhoneBook::operator = (const PhoneBook& objToCopy)
+void PhoneBook::searchPB(string phoneSearch) 
 {
-	cout << "OAO here";
-	numContacts = objToCopy.numContacts;
-	capacity = objToCopy.capacity;
-	numContacts = objToCopy.numContacts;
-	for (int i = 0; i < numContacts; i++) {
-		personArr[i] = objToCopy.personArr[i];
-	}
-	return *this;
-}
-
-/*Person PhoneBook::searchPB(string search)
-{
+	//sort first in order to search for a person
 	cout << "Searching...\n" ;
+	bubbleSort(this->personArr,numContacts);
 
-
+	//search
+	//binarySearch(numContacts, 0, phoneSearch);
+	
+				
 }
 
-template<typename TYPE>
-TYPE searchHelp(TYPE arrSearch, int lowVal, int highVal, string FindMatch)
+
+/*int PhoneBook::binarySearch(int lowVal, int highVal, string itemMatch)
 {
 		int midVal;        // Midpoint of low and high values
 		int itemPos;       // Position where item found, -1 if not found
@@ -80,35 +88,38 @@ TYPE searchHelp(TYPE arrSearch, int lowVal, int highVal, string FindMatch)
 		rangeSize = (highVal - lowVal) + 1;
 		midVal = (highVal + lowVal) / 2;
 
-		if (itemMatch == stringsList.at(midVal)) {   // Base case 1: item found at midVal position
+		if (itemMatch == personArr[midVal].getPhoneNumber()) {   // Base case 1: item found at midVal position
 			itemPos = midVal;
 		}
 		else if (rangeSize == 1) {                   // Base case 2: match not found
 			itemPos = -1;
 		}
 		else {                                       // Recursive case: search lower or upper half
-			if (itemMatch < stringsList.at(midVal)) { // Search lower half, recursive call
-				itemPos = FindMatch(stringsList, itemMatch, lowVal, midVal);
+			if (itemMatch < personArr[midVal].getPhoneNumber()) { // Search lower half, recursive call
+				itemPos = binarySearch(lowVal, midVal, itemMatch);
 			}
 			else {                                    // Search upper half, recursive call
-				itemPos = FindMatch(stringsList, itemMatch, midVal + 1, highVal);
+				itemPos = binarySearch(midVal + 1, highVal, itemMatch);
 			}
 		}
 
-		//return itemPos;
-}
+		return itemPos;
+}*/
 
-template<typename TYPE>
-void bubbleSort(TYPE values, int size)
+//sorting by phone number
+void PhoneBook::bubbleSort(CongressPerson values[], int size)
 {
-	TYPE i, j, temp;
+	int i, j;
+	CongressPerson temp;
 	int countComparisons = 0;
 	bool continueSorting = true;
 	for (i = 0; i < size - 1 && continueSorting; i++) {
-		//continueSorting=false;
+		//continueSorting=false;     		
+  
 
 		for (j = 0; j < size - i - 1; j++) {
-			if (values[j].getPhoneNumber() > values[j + 1].getPhoneNumber) {
+			if (values[j] > values[j + 1]) { //comparing phone numbers
+				//swaps congressPersons
 				temp = values[j];
 				values[j] = values[j + 1];
 				values[j + 1] = temp;
@@ -117,9 +128,26 @@ void bubbleSort(TYPE values, int size)
 			}
 			countComparisons++;
 		}
-		cout << i << ": ";
-		printArray(values, size);
 	}
-	cout << countComparisons << endl;
 }
-*/
+
+PhoneBook& PhoneBook::operator = (const PhoneBook& objToCopy)
+{
+	cout << "OAO here";
+
+	delete[] personArr;
+
+	numContacts = objToCopy.numContacts;
+	capacity = objToCopy.capacity;
+
+	personArr = new CongressPerson[capacity];
+
+	numContacts = objToCopy.numContacts;
+	for (int i = 0; i < numContacts; i++) {
+		personArr[i] = objToCopy.personArr[i];
+	}
+	return *this;
+}
+
+
+
